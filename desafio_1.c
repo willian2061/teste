ca5b4b9dff1e70ddb21227a5f2e7f136db9483e2
willian2061 <willianchen2061@gmail.com>
2021-09-03 20:26:56 -0300

    teste de commandos com atividades antigas

---
+++ desafio_1.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+main()
+{
+float n1,n2,n3,n4,n5,n6,n7,n8,n9,n10,res; //variavel dos 10 numeros e o resultado
+
+printf("informe o primeiro numero: "); //recepcao dos 10 numeros
+scanf("%f",&n1);
+printf("informe o segundo numero: ");
+scanf("%f",&n2);
+printf("informe o terceiro numero: ");
+scanf("%f",&n3);
+printf("informe o quarto numero: ");
+scanf("%f",&n4);
+printf("informe o quinto numero: ");
+scanf("%f",&n5);
+printf("informe o sexto numero: ");
+scanf("%f",&n6);
+printf("informe o setimo numero: ");
+scanf("%f",&n7);
+printf("informe o oitavo numero: ");
+scanf("%f",&n8);
+printf("informe o nono numero: ");
+scanf("%f",&n9);
+printf("informe o decimo numero: ");
+scanf("%f",&n10);
+	res=n1+n2+n3+n4+n5+n6+n7+n8+n9+n10; //soma dos 10 numeros
+		printf("o resultado e %f\n", res); //mostra os resultados da soma
+
+system("pause");
+}
+//willian yu chen RA:420109998
